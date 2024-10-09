ed0eac2951df1946425410243a999ce35d622060
TWAN KUIPERS <twankuipers@home.nl>
2024-10-09 14:44:47 +0200

    Updated collapsible sidebar elements

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
+
 	const links = document.querySelectorAll('.sidebar ul li a');
 	links.forEach(link => {
 		link.addEventListener('click', function(e) {
 			e.preventDefault();
-			const targetId = this.getAttribute('href').substring(1);
-			const targetSection = document.getElementById(targetId);
-			targetSection.scrollIntoView({ behavior: 'smooth' });
+
+			// Only scroll to the page if it is either not a collapsable tag
+			// or if it is a collapsable tag and we are opening it
+			if (!link.classList.contains('collapsible') || !link.classList.contains('active')) {
+				const targetId = this.getAttribute('href').substring(1);
+				const targetSection = document.getElementById(targetId);
+				targetSection.scrollIntoView({ behavior: 'smooth' });
+			}
 		});
 	});
 
@@ -18,9 +24,11 @@ document.addEventListener('DOMContentLoaded', function() {
 			if (nestedList.style.display === 'block') {
 				nestedList.style.display = 'none';
 				this.classList.remove('active');
+				link.querySelector('span.plus').textContent = "+";
 			} else {
 				nestedList.style.display = 'block';
 				this.classList.add('active');
+				link.querySelector('span.plus').textContent = "‒";
 			}
 		});
 	});
